6bae36e3910b37b94d4704b2fdfbb6711d3aedea
Aakash12-hub <20erece001.aakash@rietjaipur.ac.in>
2022-02-17 14:25:06 +0530

    hh

---
+++ bank account/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name = 'gillu', # while installing pacakge, e.g., matplotlib...
+    version = '0.0.1',
+    description = 'Simple Bank Account.',
+    long_description = open('Readme.txt').read(),
+    url = 'https://pypi.org/user/imvickykumar999/',
+    author = 'aakash verma',
+    keywords = ['account', 'bank', 'class', 'ideationology'],
+    packages = ['vicks'], # while importing package, e.g., pyplot...
+)
---
+++ bank account/vicks/__init__.py
@@ -0,0 +1,1 @@
+print('PYPI Profile : https://pypi.org/user/imvickykumar999/')
